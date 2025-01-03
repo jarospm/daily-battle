c85135485ce5b7075e58731400a1be1836bfd985
Jaros <jaros@chimplie.com>
2025-01-03 18:14:34 +0100

    add winner highlight

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // DOM REFERENCES
 
-let displayRight = document.querySelector(".score-box.right");
-let displayWrong = document.querySelector(".score-box.wrong");
+const displayRight = document.querySelector(".score-box.right");
+const displayWrong = document.querySelector(".score-box.wrong");
+const rightSide = document.querySelector(".score-box.right").parentElement;
+const wrongSide = document.querySelector(".score-box.wrong").parentElement;
 
 
 // SCORE TRACKER
@@ -12,11 +14,13 @@ let scoreWrong = 0;
 function plusRight(n) {
     scoreRight += n;
     displayRight.textContent = scoreRight;
+    highlightWinner();
 }
 
 function plusWrong(n) {
     scoreWrong += n;
     displayWrong.textContent = scoreWrong;
+    highlightWinner();
 }
 
 function resetScores() {
@@ -24,4 +28,25 @@ function resetScores() {
     scoreWrong = 0;
     displayRight.textContent = scoreRight;
     displayWrong.textContent = scoreWrong;
+    highlightRemove();
+}
+
+
+// HIGHLIGHT WINNER
+
+function highlightWinner() {
+    highlightRemove();
+
+    if (scoreRight == scoreWrong) {
+        highlightRemove();
+    } else if (scoreRight > scoreWrong) {
+        rightSide.classList.add("winning");
+    } else {
+        wrongSide.classList.add("winning");
+    }
+}
+
+function highlightRemove() {
+    rightSide.classList.remove("winning");
+    wrongSide.classList.remove("winning");
 }
