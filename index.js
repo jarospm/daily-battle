c964453485556c88003dfaf0ae955eab08a05364
Jaros <jaros@chimplie.com>
2025-01-03 16:29:14 +0100

    add basic score tracker

---
+++ index.js
@@ -0,0 +1,20 @@
+// DOM REFERENCES
+
+let displayRight = document.querySelector(".score-box.right");
+let displayWrong = document.querySelector(".score-box.wrong");
+
+
+// SCORE TRACKER
+
+let scoreRight = 0;
+let scoreWrong = 0;
+
+function plusRight(n) {
+    scoreRight += n;
+    displayRight.textContent = scoreRight;
+}
+
+function plusWrong(n) {
+    scoreWrong += n;
+    displayWrong.textContent = scoreWrong;
+}
